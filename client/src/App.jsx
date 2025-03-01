dd7e9bd2b482d616c161a41c2478b80ada223274
cdelzell <claradelzell@gmail.com>
2025-03-01 12:07:22 -0600

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,34 @@ import { socket } from "./SocketFactory";
 import nightSky from "./assets/night_sky.jpg";
 import Welcome from "./components/Welcome";
 import Star from "./components/Star";
+<<<<<<< Updated upstream
 import CustomizeStar from "./components/CustomizeStar";
 import "./App.css";
 
 function App() {
+=======
+import "./App.css";
+
+function App() {
+  const [floatingObjects, setFloatingObjects] = useState([]);
+  const [componentPosition, setComponentPosition] = useState(null);
+  const [appState, setAppState] = useState("view");
+
+  if (appState === "setStar") {
+  }
+
+  // Function to handle the click event
+  const handleClick = (event) => {
+    const x = event.clientX; // X position of click
+    const y = event.clientY; // Y position of click
+
+    setAppState("setStar");
+
+    // Set the position of the component to render
+    setComponentPosition({ x, y });
+  };
+
+>>>>>>> Stashed changes
   return (
     <div
       style={{
@@ -17,6 +41,7 @@ function App() {
         minHeight: "100vh",
         width: "100vw",
       }}
+      onClick={handleClick}
     ></div>
   );
 }
