879de10e29b503959a01a9aaea6b7008516aff4d
rmunjewar <riddhismunjewar@gmail.com>
2025-03-01 11:24:51 -0600

    made array of elements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import { socket } from './SocketFactory'
 import nightSky from './assets/night_sky.jpg';
+import nightSky from './assets/corn.jpg';
+import nightSky from './assets/cow.jpg';
+import nightSky from './assets/ufo.jpg';
+import nightSky from './assets/astronaut.jpg';
+import nightSky from './assets/moon.jpg';
 import './App.css'
 
+// array of images that randomly go across the sky
+const images = [corn, cow, ufo, astronaut, moon]
+
 function App() {
+
+  const [floatingObjects, setFloatingObjects] = useState([]);
   
   return (
+
     <div 
       style = {{
         backgroundImage: `url(${nightSky})`,
