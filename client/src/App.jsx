269c1a28b515376856fffbd7fd812dbcaa915996
cdelzell <claradelzell@gmail.com>
2025-03-01 12:06:51 -0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,15 @@
-import { useState } from "react"
-import { socket } from "./SocketFactory"
+import { useState } from "react";
+import { socket } from "./SocketFactory";
 import nightSky from "./assets/night_sky.jpg";
-import nightSky from './assets/corn.jpg';
-import nightSky from './assets/cow.jpg';
-import nightSky from './assets/ufo.jpg';
-import nightSky from './assets/astronaut.jpg';
-import nightSky from './assets/moon.jpg';
 import Welcome from "./components/Welcome";
 import Star from "./components/Star";
-<<<<<<< HEAD
-import './App.css'
-=======
 import CustomizeStar from "./components/CustomizeStar";
 import "./App.css";
->>>>>>> 59ae408f19c59d4f5dee66a57bae13b1bd405aeb
 
 function App() {
-
-  const [floatingObjects, setFloatingObjects] = useState([]);
-  
   return (
-<<<<<<< HEAD
-
-    <div 
-      style = {{
+    <div
+      style={{
         backgroundImage: `url(${nightSky})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
@@ -32,10 +18,15 @@ function App() {
         width: "100vw",
       }}
     ></div>
-=======
-    <div></div>
->>>>>>> 59ae408f19c59d4f5dee66a57bae13b1bd405aeb
   );
+}
+
+function renderObjects() {
+  <div>
+    {floatingObjects.map((object, index) => (
+      <div key={index} x={x} y={y} />
+    ))}
+  </div>;
 }
 
 export default App;
