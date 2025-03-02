dafe74bf531a70283fe8b9563cb98bc05979ad62
cdelzell <claradelzell@gmail.com>
2025-03-02 09:40:27 -0600

    fewa

--- client/src/components/Music.jsx
+++ client/src/components/Music.jsx
@@ -21,8 +21,8 @@ function Music() {
   return (
     <div>
       <button className="playMusic" onClick={handlePlayMusic}>
-        {!play && <img src="./src/assets/sound-off.png" alt="play" />}
-        {play && <img src="./src/assets/sound-high.png" alt="pause" />}
+        {!play && <img src="./assets/sound-off.svg" alt="play" />}
+        {play && <img src="./assets/sound-high.svg" alt="pause" />}
       </button>
       <audio
         ref={audioRef}
