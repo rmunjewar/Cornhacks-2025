c89ede43d1cc2e4028efe418863fe4dbad0c70ac
cwelstead <welstead.cole@gmail.com>
2025-03-01 23:07:07 -0600

    frontend troubleshooting

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Ensure correct routing
+  server: {
+    proxy: {
+      "/socket.io": {
+        target: "https://a-brighter-future.onrender.com",
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  },
+});
