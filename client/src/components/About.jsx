0db144ac4e2cdcdfbdb8cbd5086e4bb15f348345
SWally19 <sam.wallenburg@gmail.com>
2025-03-01 20:19:56 -0600

    Finish it up, fix some stuff yuhh

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -7,28 +7,37 @@ function About() {
 
     return (
         <div>
-            <div className="popUp" hidden={!showAbout}>
-                <div className="heading">
-                    A BRIGHTER FUTURE
+            {showAbout && (
+                <div>
+                    <div className="heading">
+                        A BRIGHTER FUTURE
+                    </div>
+                    <div className="subheading">
+                        Our Mission
+                    </div>
+                    <div className="description">
+                        mission statement here
+                    </div>
+                    <div className="subheading">
+                        How It Works
+                    </div>
+                    <div className="description">
+                    1. Click anywhere on the night sky to place a star.
+                    </div>
+                    <div className="description">
+                    2. Customize your star by choosing its color, size, and brightness. 
+                    </div>
+                    <div className="description">
+                    3. Enjoy the cosmic view! 
+                    </div>
                 </div>
-                <div className="subheading">
-                    Our Mission
-                </div>
-                <div className="description">
-                    mission statement here
-                </div>
-                <div className="subheading">
-                    How It Works
-                </div>
-                <div className="description">
-                    instructions here
-                </div>
+            )}
+            <div className="aboutButton"
+                onMouseEnter={() => setShowAbout(true)}
+                onMouseLeave={() => setShowAbout(false)}>
+                ?
             </div>
-            
-                <div className="aboutButton" onClick={() => setShowAbout(!showAbout)}>
-                    ?
-                </div>
-           
+
         </div>
     );
 }
