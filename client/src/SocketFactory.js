885a4639b4b6349e10e836d94d6fdab79599289a
cwelstead <welstead.cole@gmail.com>
2025-03-01 23:13:01 -0600

    forces websocket instead of polling

--- client/src/SocketFactory.js
+++ client/src/SocketFactory.js
@@ -3,6 +3,6 @@ import { io } from 'socket.io-client'
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://a-brighter-future.onrender.com'
 
 export const socket = io(URL, {
-    transports: ["websocket", "polling"], // Ensure fallback works
+    transports: ["websocket"], // Ensure fallback works
     withCredentials: true, // Send credentials if needed
 })
